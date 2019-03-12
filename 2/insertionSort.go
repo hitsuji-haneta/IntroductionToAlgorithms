eba6894036ef156dfe5dc494755b18459f819b57
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-03-12 23:43:11 +0900

    refactor insertionSort

--- 2/insertionSort.go
+++ 2/insertionSort.go
@@ -3,25 +3,24 @@ package main
 import "fmt"
 
 func insertionSort(array []int) {
-	fmt.Println(array)
-	for i := 1; i < len(array); i++ {
-		key := array[i]
-		j := i - 1
+	for index, value := range array {
+		j := index - 1
 		for {
-			if j >= 0 && array[j] > key {
+			if j >= 0 && array[j] > value {
 				array[j+1] = array[j]
 				j = j - 1
 			} else {
+				array[j+1] = value
 				break
 			}
 		}
-		array[j+1] = key
 	}
 }
 
 func main() {
-	array := []int{3, 4, 10, 6, 2}
-	// array := []int{5, 2, 4, 6, 1, 3}
+	// array := []int{3, 4, 10, 6, 2}
+	array := []int{5, 2, 4, 6, 1, 3}
+	fmt.Println(array)
 	insertionSort(array)
 	fmt.Println(array)
 }
